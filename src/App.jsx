77edaa7b3b670dfcfa280b6bf47f08202d76fc84
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-12 17:44:20 -0400

    More cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
-export default function App (props) {
+export default function App () {
 
   return (
     <div className="boxes">
